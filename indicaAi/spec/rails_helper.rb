261088badc29c2dc463c608b6443d8b749be7206
Iasmin Mendes <mendesiasmin96@gmail.com>
2018-09-09 15:25:17 -0300

    Config rspec tests and coveralls

--- indicaAi/spec/rails_helper.rb
+++ indicaAi/spec/rails_helper.rb
@@ -9,6 +9,9 @@ require 'rspec/rails'
 
 require 'database_cleaner'
 
+require 'coveralls'
+Coveralls.wear!
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -32,6 +35,15 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -60,29 +72,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  #
-  Shoulda::Matchers.configure do |config|
-      config.integrate do |with|
-          with.test_framework :rspec
-          with.library :rails
-      end
+
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
   end
 
-  RSpec.configure do |config|
-    # add `FactoryBot` methods
-    config.include FactoryBot::Syntax::Methods
-
-    # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-    config.before(:suite) do
-      DatabaseCleaner.clean_with(:truncation)
-      DatabaseCleaner.strategy = :transaction
-    end
-
-    # start the transaction strategy as examples are run
-    config.around(:each) do |example|
-      DatabaseCleaner.cleaning do
-        example.run
-      end
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
     end
   end
 end
