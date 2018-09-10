b7338d9cd1b37820bbdc4ed1222ab95665659829
zarbielli <jlfz06@gmail.com>
2018-09-10 15:38:34 -0300

    Co-authored-by: Zarbielli <jlfz06@gmail.com> Co-authored-by: heronsousa <heronrsousa@hotmail.com>

--- indicaAi/config/routes.rb
+++ indicaAi/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get '/locals', to: 'locals#allLocals'
+  get '/locals/name/:name', to: 'locals#searchLocals'
+  
 end
