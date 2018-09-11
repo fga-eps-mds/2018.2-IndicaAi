7d810ea8da8f19b60423671d58c18b03b8a49828
zarbielli <jlfz06@gmail.com>
2018-09-11 19:35:27 -0300

    #86 - Adding validate logic
    Co-authored-by: heronsousa <heronrsousa@hotmail.com>

--- indicaAi/app/models/local.rb
+++ indicaAi/app/models/local.rb
@@ -1,5 +1,7 @@
 # Description of Places Class
 class Local < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :RatingRate, presence: true
   def self.find_by_name(params)
     Local.where('upper(name) = ?', params.upcase)
   end
