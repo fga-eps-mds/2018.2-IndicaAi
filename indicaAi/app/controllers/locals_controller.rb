df8c876a2532784adb2c60b63372184633af70f4
Gabriel <gdavi.gd@gmail.com>
2018-09-18 17:38:37 -0300

    #54 - Adding local and rating into local json
    Co-authored-by: Zarbielli <jlfz06@gmail.com>
    Co-authored-by: mendesiasmin <mendesiasmin96@gmail.com>

--- indicaAi/app/controllers/locals_controller.rb
+++ indicaAi/app/controllers/locals_controller.rb
@@ -3,18 +3,25 @@ class LocalsController < ApplicationController
   # GET /locals
   def index
     locals = Local.all
-    json_response(locals)
+    result = []
+    locals.each do |local|
+    result << locals.as_json(methods: [:local_ratings])
+    end
+    json_response(result)
   end
 
   # GET /locals/name/:name
   def search_locals
     locals = Local.find_by_name(params[:name])
-    json_response(locals)
+    result = []
+    result << locals.as_json(methods: [:local_ratings])
+    json_response(result)
   end
 
   def show_rating
     rating = Local.find_local_ratings(params[:id_local])
-    json_response(rating)
+    locals = Local.find(params[:id_local])
+    json_response(locals: locals,rating: rating,)
   end
 
 end
