a1531d3c0ab26f35847a0ccfc9b73eeb1d2fcd23
zarbielli <jlfz06@gmail.com>
2018-09-20 15:44:05 -0300

    Adding test of Local model
    Co-authored-by: GabrielDVpereira <gdavi.gd@gmail.com>

--- indicaAi/spec/models/local_spec.rb
+++ indicaAi/spec/models/local_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Local, type: :model do
   it { should validate_presence_of(:name) }
+
+  it 'Testing Search By Name - Should return similar names to params' do
+    params = 'FGA'
+    local = Local.new
+    local.name = 'FGA'
+    expect(Local.find_by_name(params)) == true
+  end
+
+  it 'Testing Find Local Ratings - Should return rating associated to parms' do
+    params = 1
+    local = Local.new
+    local.name = 'FGA'
+    local.save
+    expect(Local.find_local_ratings(params)) == true
+  end
+
 end
+
