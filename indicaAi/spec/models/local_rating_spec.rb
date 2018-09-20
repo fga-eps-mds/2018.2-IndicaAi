7cef18707a39fb685480b2b59dc142c1844d7ab1
zarbielli <jlfz06@gmail.com>
2018-09-20 14:47:27 -0300

    #54 - Adding test of local_rating model
    Co-authored-by: GabrielDVpereira <gdavi.gd@gmail.com>

--- indicaAi/spec/models/local_rating_spec.rb
+++ indicaAi/spec/models/local_rating_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe LocalRating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Testing if value is integer - Should return true for value.class == Integer' do
+    note1 = LocalRating.new
+    note1.value = 3
+    assert note1.value.class == Integer 
+  end  
+
+  it 'Testing typecast - Should return integer if input is float' do
+    note2 = LocalRating.new
+    note2.value = 3.4
+    assert note2.value == 3
+  end
+  
+  it { should belong_to(:local) }
+
+  it { should validate_presence_of(:value) }
+
+  it { should validate_numericality_of(:value) }
+
+  it { should validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
+
+  it { should validate_numericality_of(:value).is_less_than_or_equal_to(5) }
 end
