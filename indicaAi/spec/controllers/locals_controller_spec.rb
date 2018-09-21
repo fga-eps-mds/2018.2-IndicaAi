fce6964b0980673c869a7070ef717f537d383004
Gabriel <gdavi.gd@gmail.com>
2018-09-21 00:52:53 -0300

    Fixed a mistake in the controller spec test

--- indicaAi/spec/controllers/locals_controller_spec.rb
+++ indicaAi/spec/controllers/locals_controller_spec.rb
@@ -1,27 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe LocalsController, type: :controller do
-    
-    let!(:local) { create(:local) }
+ 
+  let!(:local) { create(:local) }
 
+  it { should route(:get, '/locals').to(action: :index) }
+  let(:action) { LocalsController.new }
 
-    it { should route(:get, '/locals').to(action: :index) }
-    let(:action)  { LocalsController.new }
+  it 'Test index - Should return all locals as JSON' do
+    get :index, format: :JSON
+    expect(response).to be_success
+  end
 
-    it 'Test index - Should return all locals as JSON' do  
-        get :index, format: :JSON
-        expect(response).to be_success
-    end
-
-
-    describe 'GET #search_locals' do
-        context 'Return a local by its name as a param' do
-            it { should route(:get, '/locals/name/Padaria').to(action: :search_locals, name: 'Padaria') }
-        end
-    end
-    describe 'GET #show_rating' do
-        context 'Return a rating by its id as a param ' do
-            it { should route(:get, '/locals/name/Padaria').to(action: :search_locals, name: 'Padaria') }
-        end
-    end
+  describe 'GET #search_locals' do
+    context 'Return a local by its name as a param' do
+      it { should route(:get, '/locals/name/P').to(action: :search_locals, name: 'P') }
+      end
+  end
+  describe 'GET #show_rating' do
+      context 'Return a rating by its id as a param ' do
+          it { should route(:get, '/locals/1/ratings').to(action: :show_rating, id_local: 1) }
+      end
+  end
 end
