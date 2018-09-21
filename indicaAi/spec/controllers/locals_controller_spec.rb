37013bbb0aa7c3aba07402fb5a33afdd07880e6c
Gabriel <gdavi.gd@gmail.com>
2018-09-21 00:25:41 -0300

    Adding tests for the controller
    Co-authored-by: Zarbielli <jlfz06@gmail.com>

--- indicaAi/spec/controllers/locals_controller_spec.rb
+++ indicaAi/spec/controllers/locals_controller_spec.rb
@@ -1,13 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe LocalsController, type: :controller do
-    it 'Test index - Should return all locals as JSON' do
+    
+    let!(:local) { create(:local) }
+
+
+    it { should route(:get, '/locals').to(action: :index) }
+    let(:action)  { LocalsController.new }
+
+    it 'Test index - Should return all locals as JSON' do  
         get :index, format: :JSON
         expect(response).to be_success
     end
 
-    it 'Test Search Locals - Should return matching locals' do
-        get :search_locals, format: :JSON
-        expect(response).to be_success
+
+    describe 'GET #search_locals' do
+        context 'Return a local by its name as a param' do
+            it { should route(:get, '/locals/name/Padaria').to(action: :search_locals, name: 'Padaria') }
+        end
+    end
+    describe 'GET #show_rating' do
+        context 'Return a rating by its id as a param ' do
+            it { should route(:get, '/locals/name/Padaria').to(action: :search_locals, name: 'Padaria') }
+        end
     end
 end
