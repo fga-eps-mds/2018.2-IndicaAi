a1bd5b2608969c34165f949d12c29c59dcc6fca1
lucas <lucasmacielaguiar@gmail.com>
2018-09-20 22:55:43 -0300

    107 adjusting errors in tests

--- indicaAi/spec/models/favorite_local_spec.rb
+++ indicaAi/spec/models/favorite_local_spec.rb
@@ -1,20 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe FavoriteLocal, type: :model do
-let! (:local) { create(:local) }
+  let! (:local) { create(:local) }
   let!(:favorite_locals) { create_list(:favorite_local, 10, local:local) }
   it 'should return favorite locals related with local' do
-    p favorite_locals
 
-  #  local_favorite = create(:favorite_local, local:local)
-   result = FavoriteLocal.find_by_local(local.id)
+    result = FavoriteLocal.find_by_local(local.id) 
     assert result == favorite_locals
   end
-  it 'should not return favorite locals related with local' do
-    p favorite_locals
 
-  #  local_favorite = create(:favorite_local, local:local)
-   result = FavoriteLocal.find_by_local(local.id)
-    assert result == favorite_locals
-  end
+  it { should validate_presence_of(:local) }
+  it { should validate_presence_of(:user_identifier) }
 end
--- indicaAi/spec/models/user_identifier_spec.rb
+++ indicaAi/spec/models/user_identifier_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe UserIdentifier, type: :model do
   it { should have_many(:favorite_locals).dependent(:destroy) }
-  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:userId) }
 end
