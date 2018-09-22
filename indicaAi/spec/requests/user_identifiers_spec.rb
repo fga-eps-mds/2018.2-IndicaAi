302521f2cfd7d2ab99246d7ea1d9eace1c9c442e
lucas <lucasmacielaguiar@gmail.com>
2018-09-22 00:31:59 -0300

    #107 creating UserIdentifier request tests related with favorites

--- indicaAi/spec/requests/user_identifiers_spec.rb
+++ indicaAi/spec/requests/user_identifiers_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 RSpec.describe 'Local API', type: :request do
   let!(:user_identifiers) { create_list(:user_identifier, 10) }
   let(:user_identifier_id) { user_identifiers.first.id }
+  let!(:favorite_locals) { create_list(:favorite_local, 10, user_identifier: user_identifiers.first)}
 
   # Test list all users
   describe 'GET /users' do
@@ -18,5 +19,36 @@ RSpec.describe 'Local API', type: :request do
     it 'returns status code 200' do
       expect(response).to have_http_status(200)
     end
+  end
+
+  describe 'GET /user/:user_id' do
+     before{
+      get "/user/#{user_identifier_id}"
+    }
+
+    it 'return user_profile in json' do
+      expect(json).not_to be_empty
+      expect(json["id"]).to eq(user_identifiers.first.id)
+      expect(json["identifier"]).to eq(user_identifiers.first.identifier)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /user/favorites/:user_id' do
+    before{
+      get "/user/favorites/#{user_identifier_id}"
+    }
+    it 'return favorites by user in json' do
+      expect(json).not_to be_empty
+      json.each do |item|
+        # p item
+        # tests of user id
+        expect(item["user_identifier_id"]).to eq(user_identifier_id)
+      end
+    end
+    
   end
 end
