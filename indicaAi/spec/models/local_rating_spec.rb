bfe58846eaf5ae4b520246995eff30f730e1f7d3
Gabriel <gdavi.gd@gmail.com>
2018-09-22 21:49:16 -0300

    Trying hard to make this be in accord with the codeclimate

--- indicaAi/spec/models/local_rating_spec.rb
+++ indicaAi/spec/models/local_rating_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe LocalRating, type: :model do
   it { should validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
-  context 'ratings validation tests' do
-    it 'should return a integer value' do
-      note = create(:local_rating)
-      assert note.value.class == Integer
-    end
 
+  describe 'testing validations' do
+    context 'ratings validation tests' do
+      let!(:note) { create(:local_rating) }
+      it 'should return a integer value' do
+        note.save
+        # note = create(:local_rating)
+        assert note.value.class == Integer
+      end
+    end
     it 'should return a integer though a float was given' do
       note2 = LocalRating.new
       note2.value = 3.4
