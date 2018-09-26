6031e4d403230bf1514724eab703608d9cdfb07d
lucas <lucasmacielaguiar@gmail.com>
2018-09-25 22:55:57 -0300

    #107 removing action user_profile and their tests

--- indicaAi/app/controllers/user_identifiers_controller.rb
+++ indicaAi/app/controllers/user_identifiers_controller.rb
@@ -2,13 +2,13 @@
 class UserIdentifiersController < ApplicationController
   # List all users
   def index
-    users = UserIdentifier.all
-    json_response(users)
+    @users = UserIdentifier.all
+    json_response(@users)
   end
 
   # list all favorites of user
   def list_favorites
-    favorites = UserIdentifier.find_favorites(params[:user_id])
-    json_response(favorites)
+    @favorites = UserIdentifier.find_favorites(params[:user_id])
+    json_response(@favorites)
   end
 end
