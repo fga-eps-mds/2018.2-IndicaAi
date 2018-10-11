b5ceabaed58b86b7822e5f1dacdf6926e7d4a93a
lucas <lucasmacielaguiar@gmail.com>
2018-10-11 20:22:19 -0300

    #201 Creating validate uniqueness of user_identifier scope local in favorites

--- indicaAi/spec/models/favorite_local_spec.rb
+++ indicaAi/spec/models/favorite_local_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe FavoriteLocal, type: :model do
-  it { should validate_presence_of(:local) }
-  it { should validate_presence_of(:user_identifier) }
+  describe 'validations' do
+    it { should validate_presence_of(:local) }
+    it { should validate_presence_of(:user_identifier) }
+    it { should validate_uniqueness_of(:user_identifier_id).scoped_to(:local_id) }
+  end
 end
