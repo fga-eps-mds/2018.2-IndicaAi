6f1d831b25c34f9fd22f17704c58343df36cc9e9
lucas <lucasmacielaguiar@gmail.com>
2018-10-12 12:01:32 -0300

    #201 Creating favorite request tests

--- indicaAi/app/controllers/favorite_locals_controller.rb
+++ indicaAi/app/controllers/favorite_locals_controller.rb
@@ -3,19 +3,23 @@ class FavoriteLocalsController < ApplicationController
   # GET /favorites
   def index
     @favorites = FavoriteLocal.all
-  #   render json: {status: 'SUCCESS', message:'Saved article', data:@favorite},status: :ok
     json_response(@favorites)
   end
 
   # Post /favorite/create
   def create
 		@favorite = FavoriteLocal.new(favorite_params)
-    @favorite.save
-
-    if @favorite.save
-      render json: {status: 'SUCCESS', message:'Saved favorite', data:@favorite},status: :ok
+    if @favorite.valid?
+      @favorite.save
+      render json: {
+        status: 'SUCCESS', message:'Saved favorite',
+        data:@favorite
+      },status: :ok
     else
-      render json: {status: 'ERROR', message:'Favorite not saved', data:@favorite.erros},status: :unprocessable_entity
+      render json: {
+        status: 'ERROR', message:'Favorite not saved'
+        # data:@favorite.erros
+      },status: :unprocessable_entity
     end
   end
   
@@ -23,14 +27,27 @@ class FavoriteLocalsController < ApplicationController
   def update
     @favorite = FavoriteLocal.find(params[:id])
     @favorite.update(favorite_params)
-    render json: {status: 'SUCCESS', message:'Updated favorite', data:@favorite},status: :ok
+
+    if @favorite.save
+      render json: {
+        status: 'SUCCESS', message:'Updated favorite',
+        data:@favorite
+      },status: :ok
+    else
+      render json: {
+        status: 'ERROR', message:'Not updated favorite',
+      },status: :unprocessable_entity
+    end
   end
 
   # Delete /favorites/destroy/:id
   def destroy
     @favorite = FavoriteLocal.find(params[:id])
-    @favorite.destroy
-    render json: {status: 'SUCCESS', message:'Deleted favorite', data:@favorite},status: :ok
+    if @favorite.destroy
+      render json: {
+        status: 'SUCCESS', message:'Deleted favorite',
+      },status: :ok
+    end
   end
   
   private
---
+++ indicaAi/spec/requests/favorite_locals_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe 'Local API', type: :request do
+  let!(:favorites_test) { create_list(:favorite_local, 10) }
+  describe 'GET /favorites' do
+    before { get '/favorites/' }
+    it 'returns favorites' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
+end
+
+RSpec.describe 'Local API', type: :request do
+  let!(:user_test) { create(:user_identifier) }
+  let!(:local_test) { create(:local) }
+  let!(:valid_params) do
+    {
+      "local_id" => local_test.id,
+      "user_identifier_id" => user_test.id,
+      "label" => Faker::Lorem.sentence,
+      "body" => Faker::Lorem.paragraph 
+    }
+  end
+  describe 'POST /favorite/create' do
+    it 'should returns success created favorite' do
+      expect {
+        post '/favorite/create', params: valid_params
+      }.to change(FavoriteLocal, :count).by(+1)
+      expect(json).not_to be_empty
+      expect(FavoriteLocal.last).to have_attributes (valid_params)
+      expect(response).to have_http_status(200)
+    end
+    it 'should returns error not created favorite' do
+      # become in invalid_params because local_id and user_identifier = nil
+      valid_params["local_id"] = nil
+      valid_params["user_idenfier_id"] = nil
+      post '/favorite/create', params: valid_params
+      expect(json).not_to be_empty
+      expect(response).to have_http_status(422)
+      expect(response).not_to be_success
+    end
+  end
+end
+
+RSpec.describe 'Local API', type: :request do
+  let!(:user_test) { create(:user_identifier) }
+  let!(:local_test) { create(:local) }
+  let!(:favorite_test) { create(:favorite_local) }
+  let!(:valid_params) do
+    {
+      "local_id" => local_test.id,
+      "user_identifier_id" => user_test.id,
+      "label" => Faker::Lorem.sentence,
+      "body" => Faker::Lorem.paragraph 
+    }
+  end
+  describe 'PATCH /favorite/update/:id' do
+    it 'should returns success updated favorite' do
+      patch "/favorite/update/#{favorite_test.id}", params: valid_params
+      expect(FavoriteLocal.last).to have_attributes (valid_params)
+      expect(json).not_to be_empty
+      expect(response).to have_http_status(200)
+    end
+    it 'should returns error not updated favorite' do
+      valid_params["local_id"] = nil
+      valid_params["user_idenfier_id"] = nil
+      patch "/favorite/update/#{favorite_test.id}", params: valid_params
+      expect(json).not_to be_empty
+      expect(response).to have_http_status(422)
+    end
+    it 'should returns error not updated favorite' do
+      # favorite.id invalid
+      patch "/favorite/update/#{favorite_test.id + 1}", params: valid_params
+      expect(json).not_to be_empty
+      expect(response).to have_http_status(404)
+    end
+  end
+end
+
+RSpec.describe 'Local API', type: :request do
+  let!(:favorite_test) { create(:favorite_local) }
+  describe 'DELETE /favorite/delete/:id' do
+    it 'should returns success deleted favorite' do
+      delete "/favorite/delete/#{favorite_test.id}"
+      expect(json).not_to be_empty
+      expect(response).to have_http_status(200)
+    end
+    it 'should returns error favorite not found' do
+      # favorite.id invalid
+      delete "/favorite/delete/#{favorite_test.id + 1}"
+      expect(json).not_to be_empty
+      expect(response).to have_http_status(404)
+    end
+  end
+end
