7930fec2519476084bafc7ee15e087fc817968f8
zarbielli <jlfz06@gmail.com>
2018-10-16 22:29:47 -0300

    Adding LocalRating new tests

--- indicaAi/spec/models/local_rating_spec.rb
+++ indicaAi/spec/models/local_rating_spec.rb
@@ -4,24 +4,44 @@ RSpec.describe LocalRating, type: :model do
   it { should validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
 
   describe 'testing validations' do
-    context 'ratings validation tests' do
-      let!(:note) { create(:local_rating) }
-      it 'should return a integer value' do
-        assert note.value.class == Integer
-      end
-    end
     it 'should return a integer though a float was given' do
       note2 = LocalRating.new
       note2.value = 3.4
       assert note2.value == 3
     end
 
+    it 'Must exist a user' do
+      local = Local.create(name: 'Teste')
+      local.save
+      rating = LocalRating.new
+      rating.value = 4
+      rating.local = local
+      assert rating.save == false
+    end
+
+    it 'Must exist a local' do
+      user = UserIdentifier.create(identifier: 345)
+      user.save
+      rating = LocalRating.new
+      rating.value = 4
+      rating.user_identifier = user
+      assert rating.save == false
+    end
+
+    it 'Must exist a value' do
+      local = Local.create(name: 'Teste')
+      local.save
+      user = UserIdentifier.create(identifier: 345)
+      user.save
+      rating = LocalRating.new
+      rating.user_identifier = user
+      rating.local = local
+      assert rating.save == false
+    end
+    it { should belong_to(:user_identifier) }
     it { should belong_to(:local) }
-
     it { should validate_presence_of(:value) }
-
     it { should validate_numericality_of(:value) }
-
     it { should validate_numericality_of(:value).is_less_than_or_equal_to(5) }
   end
 end
