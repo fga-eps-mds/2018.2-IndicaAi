5fb5f55412c5e6a279e637e407bdaa535df6ef9c
lucas <lucasmacielaguiar@gmail.com>
2018-10-18 16:25:36 -0300

    #57 Adding local test request

--- indicaAi/spec/requests/locals_spec.rb
+++ indicaAi/spec/requests/locals_spec.rb
@@ -30,3 +30,31 @@ RSpec.describe 'Local API', type: :request do
     end
   end
 end
+
+RSpec.describe 'Local API', type: :request do
+  let!(:category_test) { create(:category) }
+  let!(:valid_params) do
+    {
+      'name' => Faker::Friends.location,
+      'category_id' => category_test.id,
+      'description' => Faker::Friends.quote,
+      'latitude' => Faker::Number.decimal(2, 8).to_f,
+      'longitude' => Faker::Number.decimal(2, 8).to_f
+    }
+  end
+  describe 'POST /local/create' do
+    it 'should returns success created local' do
+      expect do
+        post '/local/create', params: valid_params
+      end.to change(Local, :count).by(+1)
+      expect(Local.last).to have_attributes(valid_params)
+      expect(response).to have_http_status(200)
+    end
+    it 'should returns error not created local' do
+      # become in invalid_params because local_id and user_identifier = nil
+      valid_params['category_id'] = nil
+      post '/local/create', params: valid_params
+      expect(response).to have_http_status(422)
+    end
+  end
+end
