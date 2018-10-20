ae168066daaf7c1f8dc5b26b9621517e5e31f7de
zarbielli <jlfz06@gmail.com>
2018-10-20 10:51:13 -0300

    #60 - Fitting test to the new logic

--- indicaAi/spec/controllers/locals_controller_spec.rb
+++ indicaAi/spec/controllers/locals_controller_spec.rb
@@ -13,10 +13,17 @@ RSpec.describe LocalsController, type: :controller do
       should route(:get, '/locals/name/x').to(action: :search_locals, name: 'x')
     end
   end
+
   context 'GET #show_place' do
     let!(:local) { create(:local, name: 'Some name') }
     it 'Should route local/1 to the corresponding action' do
       should route(:get, '/local/1').to(action: :show_place, local_id: '1')
     end
+  end
+
+  it { should route(:get, '/local/1').to(action: :show_place, local_id: '1') }
+  it 'Should return status 404' do
+    get :show_place, params: { 'local_id' => '45' }
+    expect(response.status).to eq(404)  
   end
 end
