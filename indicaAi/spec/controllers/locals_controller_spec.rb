b0dac52e3f7fa849a655fd7deca68a5aee257609
zarbielli <jlfz06@gmail.com>
2018-10-20 10:59:45 -0300

    #60 - Fitting to style sheet

--- indicaAi/spec/controllers/locals_controller_spec.rb
+++ indicaAi/spec/controllers/locals_controller_spec.rb
@@ -24,6 +24,6 @@ RSpec.describe LocalsController, type: :controller do
   it { should route(:get, '/local/1').to(action: :show_place, local_id: '1') }
   it 'Should return status 404' do
     get :show_place, params: { 'local_id' => '45' }
-    expect(response.status).to eq(404)  
+    expect(response.status).to eq(404)
   end
 end
