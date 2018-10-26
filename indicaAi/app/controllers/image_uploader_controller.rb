15c9c5a6ca76d5ecb6d80b382f9ccff52a693c60
zarbielli <jlfz06@gmail.com>
2018-10-26 16:55:56 -0300

    Fitting to stylesheet

--- indicaAi/app/controllers/image_uploader_controller.rb
+++ indicaAi/app/controllers/image_uploader_controller.rb
@@ -1,12 +1,10 @@
 # Image uploader controller
 class Api::V1::UploadsController < ApplicationController
-
-    def create
-      newImage = Upload.new
-      newImage.file = params["uploaded_image"]
-      newImage.user = current_user
-      if newImage.save
-        render json: Upload.last
-      end
+  def create
+    new_image = Upload.new
+    new_image.file = params['uploaded_image']
+    new_image.user = current_user
+    render json: Upload.last if new_image.save
     end
   end
+end
