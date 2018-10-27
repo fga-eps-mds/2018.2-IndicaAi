840112c2e6d5a9141a3b5352a89881c1be3e8286
zarbielli <jlfz06@gmail.com>
2018-10-27 13:13:45 -0300

    #264 - Adding gem CarrierWave to envirioment

--- indicaAi/db/schema.rb
+++ indicaAi/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024041749) do
+ActiveRecord::Schema.define(version: 20181027135859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20181024041749) do
     t.float "longitude"
     t.string "address"
     t.string "telephone"
+    t.string "image"
   end
 
   create_table "opening_hours", force: :cascade do |t|
