9acd1b7519221cba13ed395d89f328679282ae58
zarbielli <jlfz06@gmail.com>
2018-11-02 11:12:47 -0300

    #264 - Fixing LocalImges's controller test

--- indicaAi/spec/controllers/local_images_controller_spec.rb
+++ indicaAi/spec/controllers/local_images_controller_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe LocalImagesController, type: :controller do
   let!(:local) { create(:local) }
   let!(:valid_params) do
+  @images = [Faker::ChuckNorris.fact,Faker::ChuckNorris.fact]
     {
       id: local.id,
-      image: Faker::StarWars.quote
+      image: @images
     }
   end
   describe 'POST #recive_image' do
@@ -16,11 +17,10 @@ RSpec.describe LocalImagesController, type: :controller do
     end
     it 'Should return ERROR, Image not saved' do
       # local_id invalid
-      valid_params['local_id'] = nil
+      valid_params['image'] = []
       post :receive_image, params: valid_params
 
-      expect(response).not_to be_success
+      expect(response).to have_http_status(422)
     end
   end
 end
-
