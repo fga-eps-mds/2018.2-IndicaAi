7b9349c055329bb8a1ff079e03e84084d1750e4d
zarbielli <jlfz06@gmail.com>
2018-11-03 00:03:07 -0300

    #264 - Removing useless mothods

--- indicaAi/app/uploaders/image_uploader.rb
+++ indicaAi/app/uploaders/image_uploader.rb
@@ -3,12 +3,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  def extension_whitelist
-    %w[jpg jpeg gif png]
-  end
 end
