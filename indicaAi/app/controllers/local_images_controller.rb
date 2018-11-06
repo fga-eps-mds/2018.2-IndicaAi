5ce3963bde29af62249b8eb968a6c899d854cc92
zarbielli <jlfz06@gmail.com>
2018-11-06 12:18:49 -0200

    #264 - Fixing indentention

--- indicaAi/app/controllers/local_images_controller.rb
+++ indicaAi/app/controllers/local_images_controller.rb
@@ -3,10 +3,10 @@ class LocalImagesController < ApplicationController
   def receive_image
     local = Local.find(params[:id])
     return response_success('ERROR', 404) unless local.present?
-      images = params[:image]
-      images.each do |image|
-        LocalImage.create(image: image, local_id: params[:id])
-      end
-      response_success('SUCCESS', 'Images saved', 200)
+    images = params[:image]
+    images.each do |image|
+      LocalImage.create(image: image, local_id: params[:id])
+    end
+    response_success('SUCCESS', 'Images saved', 200)
   end
 end
